8a0344125f53b252d13fa129443eade20a60ba4f
Egor Laptev <egor_laptev@epam.com>
2021-10-11 19:54:22 +0300

    start point to test

--- src/main/java/ru/codinggym/controllers/MainController.java
+++ src/main/java/ru/codinggym/controllers/MainController.java
@@ -1,22 +1,25 @@
 package ru.codinggym.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.codinggym.model.User;
 import ru.codinggym.model.UserRepository;
+import ru.codinggym.service.CurrencyService;
 import ru.codinggym.service.GreetingsService;
 
 import java.util.Optional;
 
 @RestController
-public class HelloController {
+public class MainController {
 
     @Autowired
     private GreetingsService greetingsService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CurrencyService currencyService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -31,5 +34,15 @@ public class HelloController {
         } else {
             return greetingsService.getGreeting();
         }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+    }
+
+    @GetMapping("/exchange")
+    public String currency(@RequestParam String currency) {
+        return currencyService.getLatestCurrency(currency);
     }
 }
