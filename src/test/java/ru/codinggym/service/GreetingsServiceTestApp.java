5f3588eaf2ea671369924321fdee4e4bd046d1a9
Egor Laptev <egor_laptev@epam.com>
2021-11-14 19:18:13 +0300

    start point to test 3

--- src/test/java/ru/codinggym/service/GreetingsServiceTestApp.java
+++ src/test/java/ru/codinggym/service/GreetingsServiceTestApp.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GreetingsServiceTest {
+class GreetingsServiceTestApp {
 
     @Test
     void getGreeting() {
